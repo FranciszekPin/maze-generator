f40bd32d48cfb9a8b8110f67b7b07ab88633a0be
Franciszek Pindel <franciszek.pin@gmail.com>
2020-12-08 23:35:11 +0100

    Edit main.py configuration

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import mazegen
 import mazeshow
 
-# edit this vals to create your custom maze
-# -------------
-TILE_SIZE = 30
-WIDTH = 30
-HEIGHT = 30
-# -------------
+# edit this variables to create your custom maze
+# --------------------
+TILE_SIZE = 30          # defines the length of the edge of the square in pixels
+NUMBER_OF_ROWS = 30     # defines number of tiles in column
+NUMBER_OF_COLUMNS = 30  # defines number of tiles in row
+# --------------------
 
-maze = mazegen.get_maze(WIDTH, HEIGHT)
+maze = mazegen.get_maze(NUMBER_OF_COLUMNS, NUMBER_OF_ROWS)
 mazeshow.show_maze(maze, TILE_SIZE)
